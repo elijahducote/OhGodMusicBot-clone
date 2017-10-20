2fa3a4b8b98d14a820f59da53c95dcb558421207
elijahducote <validpupflower@gmail.com>
2017-10-20 16:33:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -95,4 +95,4 @@ client.on('message', msg => {
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
 });
 client.login(tokens.d_token);
-client.login(process.env.BOT_TOKEN):
+client.login(process.env.BOT_TOKEN);
